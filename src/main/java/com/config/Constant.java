1cb5f9380a11f9fdfd6e37a2f521b6bc09d9b872
StephenJWu <limen.stephen@gmail.com>
2020-02-10 20:56:13 +0800

    初始化，端口修改为了9080

---
+++ src/main/java/com/config/Constant.java
@@ -0,0 +1,15 @@
+package com.config;
+
+/**
+ * 项目中的常量定义类
+ */
+public class Constant {
+    /**
+     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
+     */
+    public static final String APP_KEY = "dinggbyduiphzyxwhu96";
+    /**
+     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
+     */
+    public static final String APP_SECRET="NadH5f7xXHcmUcDDwLE-IiwJnF5gXTm1Kt9PZMucD-zhH_S4yvfKBLbWH6s2qlhK";
+}
