5a4bb587c8b317712a299022c821ae2af0573f11
StephenJWu <limen.stephen@gmail.com>
2020-04-20 18:13:09 +0800

    切换到ICBC企业
    切换应用信息

--- src/main/java/com/config/DingDingKeyConstant.java
+++ src/main/java/com/config/DingDingKeyConstant.java
@@ -7,9 +7,9 @@ public class DingDingKeyConstant {
     /**
      * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
      */
-    public static final String APP_KEY = "dinggbyduiphzyxwhu96";
+    public static final String APP_KEY = "ding34khq35nmzptjauz";
     /**
      * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
      */
-    public static final String APP_SECRET="NadH5f7xXHcmUcDDwLE-IiwJnF5gXTm1Kt9PZMucD-zhH_S4yvfKBLbWH6s2qlhK";
+    public static final String APP_SECRET="2hafn1ULPRaE4Lw5blHSc0ts1CR_JFkvlIGK7gCma543vMMCXsOTs2zEhWbX-8w0";
 }
